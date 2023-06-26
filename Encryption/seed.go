66cea886d7b87bc3b7cf65ff2d1d9d94a98d5a4e
Guy Vider <guy@guyvider.com>
2023-06-26 16:02:27 -0700

    start working on ETH address

--- Encryption/seed.go
+++ Encryption/seed.go
@@ -1,9 +1,14 @@
-package Encryption
+package encryption
 
 import (
+	"crypto/ecdsa"
 	"crypto/sha256"
+	"fmt"
+	"log"
 	"strings"
 
+	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/tyler-smith/go-bip39"
 )
 
@@ -19,4 +24,26 @@ func GetMnemonic(entropy []byte) ([]string, error) {
 		return nil, err
 	}
 	return strings.Split(mnemonic, " "), nil
+}
+
+func GetEthereumAddress() {
+	privateKey, err := crypto.GenerateKey()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	privateKeyBytes := crypto.FromECDSA(privateKey)
+	fmt.Println("SAVE BUT DO NOT SHARE THIS (Private Key):", hexutil.Encode(privateKeyBytes))
+
+	publicKey := privateKey.Public()
+	publicKeyECDSA, ok := publicKey.(*ecdsa.PublicKey)
+	if !ok {
+		log.Fatal("cannot assert type: publicKey is not of type *ecdsa.PublicKey")
+	}
+
+	publicKeyBytes := crypto.FromECDSAPub(publicKeyECDSA)
+	fmt.Println("Public Key:", hexutil.Encode(publicKeyBytes))
+
+	address := crypto.PubkeyToAddress(*publicKeyECDSA).Hex()
+	fmt.Println("Address:", address)
 }
